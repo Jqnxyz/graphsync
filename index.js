8d0a8ff5674c7410c0cbd11bd1f7d93f3c5f1946
Zen Quah <me@jqnxy.xyz>
2024-01-16 03:18:25 +0800

    Add call to GitHub graphQL endpoint

--- index.js
+++ index.js
@@ -2,8 +2,48 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+    // Load the inputs
     const sourceUsername = core.getInput('source-graph-username');
-    console.log(`Fetching graph from ${sourceUsername}!`);
+    const sourceToken = core.getInput('source-graph-token');
+
+    console.log(`Fetching graph from ${sourceUsername}`);
+
+    const githubGraphQLEndpoint = "https://api.github.com/graphql"
+    const contributionHistoryQuery = `query($sourceUsername:String!) { 
+                                        user(login: $sourceUsername){
+                                            contributionsCollection {
+                                                contributionCalendar {
+                                                    totalContributions
+                                                    weeks {
+                                                        contributionDays {
+                                                            contributionCount
+                                                            date
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }`
+
+    let contributionHistoryWeeks = []
+    fetch(githubGraphQLEndpoint, {
+        method: "POST",
+        headers: {
+            "Authorization": "Bearer " + sourceToken,
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            query: contributionHistoryQuery,
+            variables: { sourceUsername },
+        }),
+
+    })
+        .then(r => r.json())
+        .then(data => contributionHistoryWeeks = data["data"]["user"]["contributionsCollection"]["contributionCalendar"]["weeks"])
+
+    console.log("Contribution history returned:", contributionHistoryWeeks)
+
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
