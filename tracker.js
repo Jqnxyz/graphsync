d7027b112d0d0252585dd785f8f764e1fbb0035c
Zen Quah <me@jqnxy.xyz>
2024-01-21 23:05:58 +0800

    Refactor commit and tracker

--- tracker.js
+++ tracker.js
@@ -1,52 +1,70 @@
 
-const getProcessableHistory = (tracker, trackerFormattedFromAPI, sourceUsername, mostRecentDate) => {
-    if (tracker) {
-
-        // From trackerFormattedFromAPI[mostRecentYear][mostRecentMonth][mostRecentDay][sourceUsername], get the number of contributions (if at all)
-        let mostRecentContributions = 0
-        if (trackerFormattedFromAPI[mostRecentYear] && trackerFormattedFromAPI[mostRecentYear][mostRecentMonth] && trackerFormattedFromAPI[mostRecentYear][mostRecentMonth][mostRecentDay] && trackerFormattedFromAPI[mostRecentYear][mostRecentMonth][mostRecentDay][sourceUsername]) {
-            // if it does exist, get the number of contributions
-            mostRecentContributions = trackerFormattedFromAPI[mostRecentYear][mostRecentMonth][mostRecentDay][sourceUsername]
-        }
-
-        // Minus the mostRecentData[sourceUsername] from the JSON file, if exists
-        let mostRecentDataFromTracker = 0
-        if (mostRecentData && mostRecentData[sourceUsername]) {
-            mostRecentDataFromTracker = mostRecentData[sourceUsername]
-        }
-
-        // Set the trackerFormattedFromAPI[mostRecentYear][mostRecentMonth][mostRecentDay][sourceUsername] to the difference, if below 0, set to 0
-        trackerFormattedFromAPI[mostRecentYear][mostRecentMonth][mostRecentDay][sourceUsername] = Math.max(0, mostRecentContributions - mostRecentDataFromTracker)
+const getProcessableHistory = (tracker, trackerFormattedFromAPI, sourceUsername, mostRecentYear, mostRecentMonth, mostRecentDay, mostRecentData) => {
+    // Early return if trackerFormattedFromAPI is empty
+    if (!tracker) {
+        return trackerFormattedFromAPI
     }
+
+    // From trackerFormattedFromAPI[mostRecentYear][mostRecentMonth][mostRecentDay][sourceUsername], get the number of contributions (if at all)
+    let mostRecentContributions = 0
+    if (trackerFormattedFromAPI?.[mostRecentYear]?.[mostRecentMonth]?.[mostRecentDay]?.[sourceUsername]) {
+        // if it does exist, get the number of contributions
+        mostRecentContributions = trackerFormattedFromAPI[mostRecentYear][mostRecentMonth][mostRecentDay][sourceUsername];
+    }
+
+    // Minus the mostRecentData[sourceUsername] from the JSON file, if exists
+    let mostRecentDataFromTracker = mostRecentData?.[sourceUsername] ?? 0;
+
+    // Set the trackerFormattedFromAPI[mostRecentYear][mostRecentMonth][mostRecentDay][sourceUsername] to the difference, if below 0, set to 0
+    trackerFormattedFromAPI[mostRecentYear][mostRecentMonth][mostRecentDay][sourceUsername] = Math.max(0, mostRecentContributions - mostRecentDataFromTracker);
+
     return trackerFormattedFromAPI
 }
 
 
 const mergeAPIHistoryWithTracker = (tracker, processableHistory, sourceUsername) => {
-    if (tracker) {
-        for (let year in processableHistory) {
-            for (let month in processableHistory[year]) {
-                for (let day in processableHistory[year][month]) {
-                    if (!tracker[year]) {
-                        tracker[year] = {}
-                    }
-                    if (!tracker[year][month]) {
-                        tracker[year][month] = {}
-                    }
-                    if (!tracker[year][month][day]) {
-                        tracker[year][month][day] = {}
-                    }
-                    if (!tracker[year][month][day][sourceUsername]) {
-                        tracker[year][month][day][sourceUsername] = 0
-                    }
-                    tracker[year][month][day][sourceUsername] += processableHistory[year][month][day][sourceUsername]
+    if (!tracker) {
+        return processableHistory
+    }
+
+    for (let year in processableHistory) {
+        for (let month in processableHistory[year]) {
+            for (let day in processableHistory[year][month]) {
+                if (!tracker[year]) {
+                    tracker[year] = {}
                 }
+                if (!tracker[year][month]) {
+                    tracker[year][month] = {}
+                }
+                if (!tracker[year][month][day]) {
+                    tracker[year][month][day] = {}
+                }
+                if (!tracker[year][month][day][sourceUsername]) {
+                    tracker[year][month][day][sourceUsername] = 0
+                }
+                tracker[year][month][day][sourceUsername] += processableHistory[year][month][day][sourceUsername]
             }
         }
-    } else {
-        tracker = processableHistory
     }
+
     return tracker
 }
 
-module.exports = { getProcessableHistory, mergeAPIHistoryWithTracker };
+const sortTrackerObject = (trackerObject) => {
+    const sortedTrackerObject = {};
+    const years = Object.keys(trackerObject).sort();
+    for (const year of years) {
+        const months = Object.keys(trackerObject[year]).sort();
+        sortedTrackerObject[year] = {};
+        for (const month of months) {
+            const days = Object.keys(trackerObject[year][month]).sort();
+            sortedTrackerObject[year][month] = {};
+            for (const day of days) {
+                sortedTrackerObject[year][month][day] = trackerObject[year][month][day];
+            }
+        }
+    }
+    return sortedTrackerObject;
+}
+
+module.exports = { getProcessableHistory, mergeAPIHistoryWithTracker, sortTrackerObject };
