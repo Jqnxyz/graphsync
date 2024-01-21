45ccaf6c37f9d93542a305f30e2cf9f6cd6cb521
Zen Quah <me@jqnxy.xyz>
2024-01-21 23:13:53 +0800

    Remove broken expression

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ async function main() {
 
         const contributionHistoryWeeks = await fetchContributionHistory(sourceUsername, sourceToken);
 
-        contributionHistoryWeeks = data["data"]["user"]["contributionsCollection"]["contributionCalendar"]["weeks"]
-
         console.log("Contribution history returned:", contributionHistoryWeeks)
 
         // Check tracker.json for progress
